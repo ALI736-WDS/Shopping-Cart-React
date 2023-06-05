66c42342644c9c071462ffb84ca6dc7f85e3f07d
ALI736-WDS <ali.sh.p736@gmail.com>
2023-06-05 17:22:44 +0330

    Uplode Completed Source Shopping-Cart-React by context

---
+++ src/components/shared/Navbar.js
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+
+// Context
+import { CartContext } from '../../context/CartContextProvider';
+
+// Icons
+import shopIcon from "../../assets/icons/shop.svg";
+
+// Style
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+
+    // const { state, dispatch } = useContext(CartContext);  //dispatch inja estrefade nemishe pas niazi nist neveshte beshe
+    const { state } = useContext(CartContext); //maghadire cartContext, state va dispatch bud ke destructure konim
+
+    return (
+        <div className={styles.mainContainer}>
+            <div className={styles.container}>
+                <Link className={styles.productLink} to="/products"> Products </Link>
+                <div className={styles.iconContainer}>
+                    <Link to="/Cart"><img src={shopIcon} alt="shop" /></Link>
+                    <span> {state.itemsCounter} </span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
